f6cacba3281a49170b56e2dc9c467814a2a46246
javicuriel <javiercuriel94@gmail.com>
2018-11-08 18:12:59 -0600

    Added flask model keras

--- api/routes/food.js
+++ api/routes/food.js
@@ -1,5 +1,6 @@
 const express = require('express');
 var request = require('request');
+var path = require('path');
 
 //express-router, a subpackage for managing different routes
 const router = express.Router();
@@ -10,7 +11,7 @@ const router = express.Router();
  * @apiGroup Food
  *
  * @apiExample Example usage:
- *		localhost:3000/food/enchiladas suizas		
+ *		localhost:3000/food/get/enchiladas suizas		
  * 
  * @apiParam {String} foodName Name of the searched food
  *
@@ -23,37 +24,34 @@ const router = express.Router();
  *
  * @apiSuccessExample Success-Response:
  *		HTTP 200 OK
- *		[
- *			{
+ *		{
+ *			"data": [
+ *				{
  *				"foodID":"food_bvxig6ybaomi9kbcp2gtea34kp1j",
  *				"foodName":"Enchiladas Suizas",
  *				"calories":166.02316602316603,
  *				"carbs":16.602316602316602,
  *				"proteins":5.019305019305019,
  *				"fats":8.880308880308881
- *			}
- *		]
+ *				}
+ *			],
+ * 			"error": null
+ *		}
  *
  * @apiErrorExample Error-Response:
  *     HTTP 404 Not Found
  *     {
+ * 		 "data": null
  *       "error": "cannot connect to food-database"
  *     }
  */
 
-//define the complete URL (/api/routes/food(:foodName)) in app.js
-router.get('/:foodName', (req, res, next) => {
-	var foodName = req.params.foodName;
+ function getFoodData(foodName,callback){
+	//  Callback responds with (data, error)
 	request('https://api.edamam.com/api/food-database/parser?ingr='+foodName+'&app_id=f43473b7&app_key=069735f1f34ba52c77f71b860808235f', function (error, response, body) {
-	    if(response.statusCode == 404)
-	    {
-	    	res.json({ error: 'cannot connect to food-database' });
-	    }
-	    else //res.send(response.body)
-	    	//https://stackoverflow.com/questions/23731869/find-items-from-json-array-using-nodejs
-	    {
+		if(response.statusCode == 404) callback(null, 'cannot connect to food-database');
+	    else{
 	    	var result = JSON.parse(response.body);
-	    	//res.send(result.hints[0].food.label);
 	    	var foodINFO = [];
 	    	for (var i in result.hints)
 	    	{
@@ -64,12 +62,40 @@ router.get('/:foodName', (req, res, next) => {
 					carbs : result.hints[i].food.nutrients.CHOCDF ? result.hints[i].food.nutrients.CHOCDF : 0,
 		    		proteins : result.hints[i].food.nutrients.PROCNT ? result.hints[i].food.nutrients.PROCNT : 0,
 		    		fats : result.hints[i].food.nutrients.FAT ? result.hints[i].food.nutrients.FAT : 0
-		    		//measure URI
+		    		
 	    		});
-	    	}
-	    	res.send(JSON.stringify(foodINFO));
+			}
+			callback(foodINFO, null);
 	    }
 	})
+ }
+
+//define the complete URL (/api/routes/food(:foodName)) in app.js
+router.get('/get/:foodName', (req, res, next) => {
+	var foodName = req.params.foodName;
+	getFoodData(foodName, (data, error) => {
+		response = {data: data, error: error};
+		if(error) res.status(404);
+		res.send(response);
+	});
+});
+
+router.get('/predict', (req, res, next) => {
+	res.sendFile('index.html', { root: './flask_server' })
+});
+
+router.post('/predict', (req, res, next) => {
+	// Acting as a proxy to sed data to flask_server
+	req.pipe(request.post('http://localhost:5000/predict',null, (err, httpResponse, body) => {
+		if(err) return res.sendStatus(500);
+		data = JSON.parse(body);
+		getFoodData(data['predictions'][0]['label'], (info, error) => {
+			if(!error){
+				data['predictions'][0]['info'] = info[0];
+			}
+			res.send(data);
+		});
+	}));
 });
 
 module.exports = router;
---
+++ flask_server/flask_server.py
@@ -0,0 +1,88 @@
+from keras.models import model_from_json
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.applications.inception_v3 import preprocess_input
+import numpy as np
+import io
+import flask
+from PIL import Image
+from base64 import b64decode
+import tensorflow as tf
+
+app = flask.Flask(__name__)
+g = tf.Graph()
+model = None
+categories = []
+
+
+def load_all():
+    global model
+    with g.as_default():
+        print("Loading model...")
+        json_file = open('model_architecture.json', 'r')
+
+        loaded_model_json = json_file.read()
+        json_file.close()
+
+        model = model_from_json(loaded_model_json)
+        model.load_weights('model_weights.h5')
+
+        load_categories()
+
+def get_processed_image(binary):
+    image = load_img(io.BytesIO(binary), target_size=(299, 299))
+    image = img_to_array(image)
+    image = np.expand_dims(image, axis=0)
+    processed_image = preprocess_input(image.copy())
+    return processed_image
+
+        
+def load_categories():
+    global categories
+    with open('categories.txt', 'r') as f:
+        for category in f:
+            categories.append(category.rstrip())
+    
+        
+
+@app.route("/")
+def index():
+    return flask.send_from_directory('.', 'index.html')
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = {"success": False, "error": None}
+    # ensure an image was properly uploaded to our endpoint
+    if flask.request.method == "POST":
+        if flask.request.files["image"]:
+            # Try reading and formating image
+            try:
+                binary = flask.request.files["image"].read()
+                image = get_processed_image(binary)
+            except Exception as e:
+                image = None
+                data['error'] = str(e)
+            # If image valid, predict
+            if image is not None:
+                try:
+                    n = int(flask.request.form['n'])
+                except:
+                    n = 5
+                with g.as_default():
+                    predictions = model.predict(image)
+                    top_n = (-predictions).argsort(axis=1)[0][:n]
+                data["predictions"] = []
+                # # Get string categories and append it to predictions
+                for i in top_n:
+                    data["predictions"].append({"label":categories[i].replace("_", " "), "probability": predictions[0][i]/1.0})
+                data["success"] = True
+    return flask.jsonify(data)
+
+
+
+
+if __name__ == "__main__":
+    load_all()
+    app.run(debug=False)
+
